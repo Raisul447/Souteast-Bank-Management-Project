428aba739f7b89a1ca41227140f1fa839486825e
Raisul Islam <138724286+Raisul447@users.noreply.github.com>
2024-06-05 18:35:43 +0600

    query.sql

---
+++ query.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE seu_bank_project;
+
+CREATE TABLE Accounts (
+    account_id INT PRIMARY KEY AUTO_INCREMENT PRIMARY,
+    account_holder_name VARCHAR(255),
+    balance DECIMAL(10, 2)
+);
+
+CREATE TABLE Transactions (
+    transaction_id INT PRIMARY KEY,
+    account_id INT,
+    transaction_type VARCHAR(10),
+    amount DECIMAL(10, 2),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
+);
